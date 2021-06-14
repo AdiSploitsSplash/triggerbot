de0bd044e81c48d0a4b1eb4cd48cd5e092f06507
AdiSploitsSplash <79107363+AdiSploitsSplash@users.noreply.github.com>
2021-06-14 23:25:07 +0300

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,9 @@
+triggerbot.MouseButton1Down:connect(function()
+	local player = game:GetService("Players").LocalPlayer
+	local mouse = player:GetMouse()
+	game:GetService("RunService").RenderStepped:Connect(function()
+		if mouse.Target.Parent:FindFirstChild("Humanoid") and mouse.Target.Parent.Name ~= player.Name then
+			mouse1press() wait() mouse1release()
+		end
+	end)
+end)
